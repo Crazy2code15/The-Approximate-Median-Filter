6950bb20d2653044b5eebc10d1b499c1b571e062
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2022-01-22 22:55:06 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -9,7 +9,7 @@ module tb;
     $dumpfile("dump.vcd"); $dumpvars;
     repeat(10)begin
       {i0,i1,i2,i3} = $random;
-      {i4,i5,i6,i7} = $random;
+      #5; {i4,i5,i6,i7} = $random;
       i8= $random;
       s = $random;
       $display("i0 = %d,i1=%d, i2=%d, i3=%d, i4=%d, i5=%d, i6=%d, i7=%d, i8=%d, s=%d, y=%d",i0,i1,i2,i3,i4,i5,i6,i7,i8,s,y);
